5c193ca056afddc117a6eba2ddb08990fbb499a3
Manohara <manohara.ph@finonyx.com>
2022-05-25 18:09:10 +0530

    Return type change

--- src/main/java/com/fin/breweryapp/web/service/BeerServiceImpl.java
+++ src/main/java/com/fin/breweryapp/web/service/BeerServiceImpl.java
@@ -6,27 +6,26 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-
 @Service
 @Slf4j
 public class BeerServiceImpl implements BeerService {
-    @Override
-    public BeerDTO getBeerById(UUID beerId) {
-        return BeerDTO.builder().id(UUID.randomUUID()).beerName("Johny Walker").beerStyle("walker").build();
-    }
+	@Override
+	public BeerDTO getBeerById(UUID beerId) {
+		return BeerDTO.builder().id(UUID.randomUUID()).beerName("Johny Walker").beerStyle("walker").build();
+	}
 
-    @Override
-    public void deleteBeerById(UUID beerId) {
-        log.info("Deleting the beer ");
-    }
+	@Override
+	public void deleteBeerById(UUID beerId) {
+		log.info("Deleting the beer ");
+	}
 
-    @Override
-    public Object updateBeer(UUID beerId) {
-        return null;
-    }
+	@Override
+	public BeerDTO updateBeer(UUID beerId) {
+		return BeerDTO.builder().id(UUID.randomUUID()).beerName("Johny Walker").beerStyle("walker").build();
+	}
 
-    @Override
-    public Object saveBeer(BeerDTO beerDTO) {
-        return null;
-    }
+	@Override
+	public BeerDTO saveBeer(BeerDTO beerDTO) {
+		return BeerDTO.builder().id(UUID.randomUUID()).beerName("Johny Walker").beerStyle("walker").build();
+	}
 }
