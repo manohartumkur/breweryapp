1c985364a3aa474016b0b45add0b767f4289efe4
Manohara <manohara.ph@finonyx.com>
2022-05-25 16:53:34 +0530

    Customer Service class

--- src/main/java/com/fin/breweryapp/web/service/CustomerService.java
+++ src/main/java/com/fin/breweryapp/web/service/CustomerService.java
@@ -1,4 +1,15 @@
-package com.fin.breweryapp.web.servicec;
+package com.fin.breweryapp.web.service;
+
+import com.fin.breweryapp.web.model.CustomerDTO;
+
+import java.util.UUID;
 
 public interface CustomerService {
+    CustomerDTO getCustomerById(UUID customerId);
+
+    CustomerDTO saveCustomer(CustomerDTO customer);
+
+    CustomerDTO updateCustomer(CustomerDTO customer, UUID customerId);
+
+    void deleteCustomer(UUID customerId);
 }
