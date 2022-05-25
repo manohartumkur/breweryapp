53c4c173f6b29d85232f1ccddb1e99c7653c1740
Manohara <manohara.ph@finonyx.com>
2022-05-25 18:08:33 +0530

    index in static folder

--- src/main/java/com/fin/breweryapp/web/controller/IndexController.java
+++ src/main/java/com/fin/breweryapp/web/controller/IndexController.java
@@ -2,13 +2,20 @@ package com.fin.breweryapp.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class IndexController {
 
 	@GetMapping({ "/", "", "/index" })
-	public String welcome() {
+	public ModelAndView welcome() {
 
-		return "index";
+		return new ModelAndView("index.html");
+	}
+
+	@GetMapping({ "/index1" })
+	public String index() {
+
+		return "index1.html";
 	}
 }
