dff7a15ae931532ab7759c43e8b529778fad54b4
Manohara <manohara.ph@finonyx.com>
2022-05-28 18:39:55 +0530

    Updated BeerDTO

--- src/main/java/com/fin/breweryapp/web/model/BeerDTO.java
+++ src/main/java/com/fin/breweryapp/web/model/BeerDTO.java
@@ -5,16 +5,41 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Version;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Entity
 public class BeerDTO {
-    private UUID id;
-    private String beerName;
-    private long upc;
-    private String beerStyle;
+	@Id
+	@GeneratedValue(generator = "UUID")
+	@GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(columnDefinition = "varchar", length = 36, updatable = false, nullable = false)
+	private UUID id;
+	@Version
+	private Integer version;
+	@CreationTimestamp
+	private OffsetDateTime createdDate;
+	@UpdateTimestamp
+	private OffsetDateTime lastModifiedDate;
+	private String beerName;
+	private BeerStyleEnum beerStyle;
+	private String upc;
+	private BigDecimal price;
+	private Integer quantityToBrew;
+	private Integer minOnHand;
 }
